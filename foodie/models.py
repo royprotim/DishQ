456840065df9794389d10b7636a6aabdbf665f0c
Protim <protimr16@gmail.com>
2018-06-06 20:03:42 +0530

    Reviews added

--- foodie/models.py
+++ foodie/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
-	rest_ID = models.IntegerField()
+	rest_ID = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=50)
 	lat = models.CharField(max_length=50)
 	lon = models.CharField(max_length=50)
@@ -12,3 +12,14 @@ class Restaurant(models.Model):
 
 	def __unicode__(self):
 		return self.name
+	
+
+class Review(models.Model):
+	rest = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+	user = models.CharField(max_length=25)
+	rating = models.DecimalField(max_digits=3,decimal_places=2)
+	rev_text = models.CharField(max_length=300)
+
+	def __unicode__(self):
+		# res = Restaurant.objects.get(rest_ID=self.rest)
+		return self.user
--- foodie/urls.py
+++ foodie/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 import requests
 from . import views
-from models import Restaurant
+from models import Restaurant,Review
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -10,8 +10,14 @@ urlpatterns = [
 
 #following script is executed once when the server starts for the very first time
 r = requests.get('https://developers.zomato.com/api/v2.1/search?entity_id=4&entity_type=city&start=0&count=20&sort=rating',headers={"user-key":"2305553b33aef82dbbac6fbda05f5bc4"})
+
 Restaurant.objects.all().delete()
 for res in r.json()["restaurants"]:
 	rest = Restaurant.objects.create(rest_ID=res["restaurant"]["R"]["res_id"],name=res["restaurant"]["name"],lat=res["restaurant"]["location"]["latitude"],lon=res["restaurant"]["location"]["longitude"],url=res["restaurant"]["url"],img_url=res["restaurant"]["featured_image"],rating=res["restaurant"]["user_rating"]["aggregate_rating"])
 	rest.save()
+	reviews = requests.get('https://developers.zomato.com/api/v2.1/reviews',params={'res_id':res["restaurant"]["R"]["res_id"],'start':0,'count':5},headers={"user-key":"2305553b33aef82dbbac6fbda05f5bc4"})
+	restaurant = Restaurant.objects.get(rest_ID=res["restaurant"]["R"]["res_id"])
+	for review in reviews.json()["user_reviews"]:
+		rev = Review.objects.create(rest=restaurant,user=review["review"]["user"]["name"],rating=review["review"]["rating"],rev_text=review["review"]["review_text"])
+		rev.save()
 	print res["restaurant"]["name"]
